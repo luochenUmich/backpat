6464275b12913bb2b125c0c4fedc9def15561ef5
dustferg <dustferg@umich.edu>
2015-11-16 14:54:45 -0500

    Added post_create and post_view pages

--- controllers/post_create.py
+++ controllers/post_create.py
@@ -6,30 +6,34 @@ post_create = Blueprint('post_create', __name__)
 
 @post_create.route('/')
 def post_route():
-    return render_template('base.html')
+	if not is_logged_in():
+		return render_template('user_login.html')
+    return render_template('post_create.html')
 
 @post_create.route('/',methods=['POST'])
 def signUp():
+    if not is_logged_in():
+		return render_template('user_login.html')
     try:
-        _userid = 1 #TD: Add in login function/check
+        _username = session['username'] #TD: Add in login function/check
         _summary = request.form['_summary']
         _description = request.form['_description']
 
         # validate the received values
-        if _userid and _summary and _description:
+        if _username and _summary and _description:
             
             # All Good, let's call MySQL
             
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.execute("insert into post (userid, summary, description, dateCreated, dateLastModified, active) values (%s, %s, %s, GETDATE(), GETDATE(), 1)", (_userid, _summary, _description))
+            cursor.execute("insert into post (username, summary, description, dateCreated, dateLastModified, active) values (%s, %s, %s, GETDATE(), GETDATE(), 1)", (_username, _summary, _description))
             cursorid = cursor.lastrowid
 
             if cursorid is not 0:
                 conn.commit()
                 return json.dumps({'message':'Post created successfully !'})
             else:
-                return json.dumps({'error':"Post could not be created: " + str(data[0])})
+                return json.dumps({'error':"Post could not be created: ")})
         else:
             return json.dumps({'html':'<span>Enter the required fields</span>'})
 
