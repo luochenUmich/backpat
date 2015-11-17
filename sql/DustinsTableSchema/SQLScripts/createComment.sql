26e1ebaca1cc24a585ac054f6a08bd001b91a91c
dustferg <dustferg@umich.edu>
2015-11-17 16:46:50 -0500

    wit '#ql will be ignored, and an empty message aborts the commit.

--- sql/DustinsTableSchema/SQLScripts/createComment.sql
+++ sql/DustinsTableSchema/SQLScripts/createComment.sql
@@ -4,14 +4,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`comment` (
   `parentCommentid` INT NULL COMMENT '',
   `username` VARCHAR(20) NULL COMMENT '',
   `deletedByDate` TIMESTAMP NULL COMMENT '',
-  `deletedByUsername` VARCHAR(20) NULL COMMENT '',
   `active` INT DEFAULT 1 COMMENT '',
   `dateCreated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '',
   `comment` MEDIUMTEXT NULL COMMENT '',
   PRIMARY KEY (`commentid`)  COMMENT '',
   INDEX `postid_idx` (`postid` ASC)  COMMENT '',
   INDEX `username_idx` (`username` ASC)  COMMENT '',
-  INDEX `deletedByUsername_idx` (`deletedByUsername` ASC)  COMMENT '',
   CONSTRAINT `postid`
     FOREIGN KEY (`postid`)
     REFERENCES `mydb`.`post` (`postid`)
@@ -19,11 +17,6 @@ CREATE TABLE IF NOT EXISTS `mydb`.`comment` (
     ON UPDATE NO ACTION,
   CONSTRAINT `username`
     FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `deletedByUsername`
-    FOREIGN KEY (`deletedByUsername`)
     REFERENCES `mydb`.`user` (`username`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
