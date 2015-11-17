281f422964a31d4632ab360fadcd7213230f6664
Derekiv <derek.eliasen@gmail.com>
2015-11-16 19:21:29 -0500

    Comments with Style
    Make the comments look nicer and and the ability to collapse and expand
    them.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,6 @@
-/* put javascript here */
+$(document).ready(function(){
+    $('.comment-button').click(function(){
+        $(this).parent().next('.comment-child').slideToggle();
+         return false;
+    });
+});
