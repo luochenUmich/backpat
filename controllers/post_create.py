1089f7391c01762f0c921a3e5fcc547e39db9382
Chen Luo <luochen@umich.edu>
2015-11-18 09:39:59 -0500

    change comment picture

--- controllers/post_create.py
+++ controllers/post_create.py
@@ -17,28 +17,24 @@ def create():
     if not is_logged_in():
         return render_template('user_login.html')
 
-    try:
-        _username = session['username'] #TD: Add in login function/check
-        _summary = request.form['_summary']
-        _description = request.form['_description']
+    _username = session['username'] #TD: Add in login function/check
+    _summary = request.form['_summary']
+    _description = request.form['_description']
 
-        # validate the received values
-        if _username and _summary and _description:
-            
-            # All Good, let's call MySQL
-            conn = mysql.connection
-            cursor = conn.cursor()
-            cursor.execute("insert into post (username, summary, description) values (%s, %s, %s)", (_username, _summary, _description))
+    # validate the received values
+    if _username and _summary and _description:
 
-            cursorid = cursor.lastrowid
+        # All Good, let's call MySQL
+        conn = mysql.connection
+        cursor = conn.cursor()
+        cursor.execute("insert into post (username, summary, description) values (%s, %s, %s)", (_username, _summary, _description))
 
-            if cursorid is not 0:
-                conn.commit()
-                return redirect(url_for('post_view.show_post') + "?postid=" + cursorid)
-            else:
-                return json.dumps({'error':"Post could not be created: "})
+        cursorid = cursor.lastrowid
+
+        if cursorid is not 0:
+            conn.commit()
+            return redirect(url_for('post_view.show_post', postid=cursorid))
         else:
-            return json.dumps({'html':'<span>Enter the required fields</span>'})
-
-    except Exception as e:
-        return json.dumps({'error':str(e)})
+            return json.dumps({'error':"Post could not be created: "})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
