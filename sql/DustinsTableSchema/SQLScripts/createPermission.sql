d1691bdfe9dcf17a644a20c3683ad9a084fd228f
dustferg <dustferg@umich.edu>
2015-11-27 12:40:00 -0500

    Ignore, trying to get sql on to database server

--- sql/DustinsTableSchema/SQLScripts/createPermission.sql
+++ sql/DustinsTableSchema/SQLScripts/createPermission.sql
@@ -1,10 +1,5 @@
 CREATE TABLE IF NOT EXISTS `mydb`.`permission` (
   `username` VARCHAR(20) NOT NULL COMMENT '',
   `adminLevel` INT ZEROFILL NULL COMMENT '',
-  PRIMARY KEY (`username`)  COMMENT '',
-  CONSTRAINT `username`
-    FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`username`)  COMMENT '')
 ENGINE = InnoDB
