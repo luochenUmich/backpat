28c0614558d9d4f27351f14b6437935bad8ccc9d
Chen Luo <luochen@umich.edu>
2015-12-09 21:12:29 -0500

    finish user profile and add link to admin console

--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,44 @@
-$(document).ready(function(){
-    $('.comment-button').click(function(){
+$(document).ready(function () {
+    $('.comment-button').click(function () {
         $(this).parent().next('.comment-child').slideToggle();
-         return false;
+        return false;
     });
 
-    $('.hide-btn').click(function(){
+    $('.hide-btn').click(function () {
         if ($(this).text() === "Hide Replies") {
             $(this).text("Show Replies");
-			$(this).siblings('.media.collapse').slideToggle();
-        } else if ($(this).text() === "Show Replies"){
+            $(this).siblings('.media.collapse').slideToggle();
+        } else if ($(this).text() === "Show Replies") {
             $(this).text("Hide Replies");
-			$(this).siblings('.media.collapse').slideToggle();
+            $(this).siblings('.media.collapse').slideToggle();
         }
     });
 
     $('#commentModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget); // Button that triggered the modal
-      var modal = $(this);
-      var recipient = button.data('id'); // Extract info from data-* attributes
-      modal.find(".modal-body input[name='parentCommentID']").val(recipient);
-      // you can add more attributes here
-  });
-  
-  $('#reportModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget); // Button that triggered the modal
-      var modal = $(this);
-      var recipient = button.data('id'); // Extract info from data-* attributes
-      modal.find(".modal-body input[name='commentid']").val(recipient);
-      // you can add more attributes here
-  });
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var modal = $(this);
+        var recipient = button.data('id'); // Extract info from data-* attributes
+        modal.find(".modal-body input[name='parentCommentID']").val(recipient);
+        // you can add more attributes here
+    });
+
+    $('#reportModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget); // Button that triggered the modal
+        var modal = $(this);
+        var recipient = button.data('id'); // Extract info from data-* attributes
+        modal.find(".modal-body input[name='commentid']").val(recipient);
+        // you can add more attributes here
+    });
+
+    $("#profile_tab li").click(function () {
+        $("#profile_tab li").removeClass('active');
+        $(this).addClass('active');
+
+        $('.tab_content').hide();
+        var selected_tab = $(this).find("a").attr("href");
+        $(selected_tab).show();
+    });
+
+
 });
 
