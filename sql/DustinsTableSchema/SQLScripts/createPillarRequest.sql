567afb8f1efd51507e70015133f41e3ff25dbd0b
dustferg <dustferg@umich.edu>
2015-12-12 17:48:18 -0500

    Changed sql a bit, sorry for the small commit again!

--- sql/DustinsTableSchema/SQLScripts/createPillarRequest.sql
+++ sql/DustinsTableSchema/SQLScripts/createPillarRequest.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`pillar_request` (
   `supportUsername` VARCHAR(20) NOT NULL,
   `requestedByUsername` VARCHAR(20) NOT NULL,
   `dateCreated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  `dateAccepted` TIMESTAMP DEFAULT NULL,
+  `dateAccepted` TIMESTAMP ,
   `reason` MEDIUMTEXT NULL, 
   `isTwoWay` bit DEFAULT 0
   PRIMARY KEY (`supportUsername`, `username`))
