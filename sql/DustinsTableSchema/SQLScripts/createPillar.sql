7fd26e197720a6ce9d4ec996c45b0cc566b37d93
dustferg <dustferg@umich.edu>
2015-12-12 14:57:07 -0500

    ame as last commit

--- sql/DustinsTableSchema/SQLScripts/createPillar.sql
+++ sql/DustinsTableSchema/SQLScripts/createPillar.sql
@@ -2,16 +2,5 @@ CREATE TABLE IF NOT EXISTS `mydb`.`pillar` (
   `username` VARCHAR(20) NOT NULL ,
   `supportUsername` VARCHAR(20) NOT NULL,
   `dateCreated` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`supportUsername`, `username`),
-  INDEX `username_idx` (`username` ASC),
-  CONSTRAINT `username`
-    FOREIGN KEY (`username`)
-    REFERENCES `mydb`.`user` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `supportUsername`
-    FOREIGN KEY (`supportUsername`)
-    REFERENCES `mydb`.`user` (`username`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`supportUsername`, `username`))
 ENGINE = InnoDB
